08d6b26d5239837caacd45ca289561da42efd8c7
prashil1 <prashil.lonakar@gmail.com>
2023-01-05 18:28:31 +0530

    new project added again

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { AutoCompleteModule } from 'primeng/autocomplete';
-import { SliderModule } from 'primeng/slider';
-
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent
+  ],
   imports: [
     BrowserModule,
-    FormsModule,
-    AppRoutingModule,
-    AutoCompleteModule,
-    SliderModule,
+    AppRoutingModule
   ],
-  exports: [AutoCompleteModule, SliderModule],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
