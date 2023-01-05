0112e297edc7b428a9f85a8a4ea0c8b8c363e181
prashil1 <prashil.lonakar@gmail.com>
2023-01-05 18:15:53 +0530

    bootstrap and ng-prime added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import { SliderModule } from 'primeng/slider';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    AutoCompleteModule,
+    SliderModule,
   ],
+  exports: [AutoCompleteModule, SliderModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
