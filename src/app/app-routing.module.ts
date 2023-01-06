efe9a8ba57931973333b177cb16855a46e7ffc60
prashil1 <prashil.lonakar@gmail.com>
2023-01-06 15:47:57 +0530

    common scss added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignInComponent } from './layout/containers/auth/sign-in/sign-in.component';
+import { MainPageComponent } from './layout/containers/main-page/main-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainPageComponent,
+    children: [],
+  },
+  {
+    path: 'sign-in',
+    component: SignInComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
