8794b97fe8765f5b8ed92bef02fd66893488d372
prashil1 <prashil.lonakar@gmail.com>
2023-01-23 16:22:28 +0530

    Main Issue fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout';
 import { SharedModule } from './shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, LayoutModule, SharedModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    LayoutModule,
+    SharedModule,
+    BrowserAnimationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/layout/index.ts
+++ src/app/layout/index.ts
@@ -3,13 +3,18 @@ import { HeaderComponent } from './components/header/header.component';
 import { MainPageComponent } from './containers/main-page/main-page.component';
 import * as fromAuthComponents from './containers/auth';
 import { SharedModule } from '../shared/shared.module';
+import { RouterModule } from '@angular/router';
 
 const layoutComponents = [HeaderComponent, MainPageComponent];
 
 @NgModule({
   declarations: [...layoutComponents, ...fromAuthComponents.components],
-  imports: [SharedModule],
-  exports: [...layoutComponents, ...fromAuthComponents.components],
+  imports: [SharedModule, RouterModule],
+  exports: [
+    ...layoutComponents,
+    ...fromAuthComponents.components,
+    RouterModule,
+  ],
   providers: [],
   bootstrap: [],
 })
